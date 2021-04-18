ac0a032c7176af7207c507ff84c67ca4d208d0ab
khadija-nur <59174800+khadija-nur@users.noreply.github.com>
2021-04-18 15:49:32 +0100

    adds stats page with pie chart

--- pages/stats.js
+++ pages/stats.js
@@ -1,10 +1,15 @@
-import Link from 'next/link';
-import Image from 'next/image';
-import { HomeTabs, Footer } from './../src/styledComponents/reusables';
+import Link from "next/link";
+import Image from "next/image";
+import { HomeTabs, Footer } from "./../src/styledComponents/reusables";
+import Charts from "../components/Charts";
+const data = [
+  { name: "Food wasted", value: 300 },
+  { name: "Food used", value: 300 },
+];
 
 export default function Home() {
   return (
-    <div className="resultCont">
+    <div className='resultCont'>
       {/* <Image
         src="/results.svg"
         alt="img"
@@ -13,24 +18,14 @@ export default function Home() {
         layout="fixed"
       /> */}
 
+      <Charts data={data} />
+
       <Footer>
-        <Link href="/">
-          <Image
-            src="/homeButton.svg"
-            alt="img"
-            width={100}
-            height={100}
-            layout="fixed"
-          />
+        <Link href='/'>
+          <Image src='/homeButton.svg' alt='img' width={100} height={100} layout='fixed' />
         </Link>
-        <Link href="/">
-          <Image
-            src="/logoutButton.svg"
-            alt="img"
-            width={100}
-            height={100}
-            layout="fixed"
-          />
+        <Link href='/'>
+          <Image src='/logoutButton.svg' alt='img' width={100} height={100} layout='fixed' />
         </Link>
       </Footer>
     </div>
