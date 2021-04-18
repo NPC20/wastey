c2936e5b2afd2914555481f3cae6b69c251c7ac2
My Hoa Tien <myhoatien@MooBook-Pro.lan>
2021-04-18 09:38:33 +0100

    Add search list to shopping page
    Co-authored-by: Jihyun Jang <jhjang742@gmail.com>

--- wastey-app/pages/shopping.js
+++ wastey-app/pages/shopping.js
@@ -1,8 +1,28 @@
-import Link from 'next/Link';
-import Image from 'next/image';
-import { HomeTabs, Footer } from './../src/styledComponents/reusables';
+import { useState } from "react";
+import {
+  updateGenericFoodList,
+  getGenericFoodList,
+  sendDataToDB,
+} from "../src/foodData";
+import Link from "next/Link";
+import Image from "next/image";
+import { HomeTabs, Footer } from "./../src/styledComponents/reusables";
 
-export default function Home() {
+export async function getStaticProps() {
+  try {
+    const updateFoodList = await updateGenericFoodList();
+    const genericFoodList = await getGenericFoodList();
+    return {
+      props: { genericFoodList },
+    };
+  } catch (e) {
+    console.log("uh oh", e);
+  }
+}
+
+export default function Home({ genericFoodList }) {
+  const [chosenItems, setChosenItems] = useState({});
+
   return (
     <div className="mainCont">
       <Image
@@ -12,6 +32,36 @@ export default function Home() {
         height={100}
         layout="fixed"
       />
+
+      <div>
+        <h1>FoodList</h1>
+        <form
+          onSubmit={(e) => {
+            e.preventDefault();
+            console.log(e.target.food.value);
+            const itemName = e.target.food.value;
+            setChosenItems({ ...chosenItems, [itemName]: 0 });
+          }}
+        >
+          <input type="text" list="food" name="food" />
+          <button type="submit">Add Item</button>
+        </form>
+        <datalist id="food">
+          {genericFoodList.fruit.map((list, index) => (
+            <option key={index} value={list}>
+              {list}
+            </option>
+          ))}
+        </datalist>
+        <ul>
+          {Object.keys(chosenItems)
+            .map((key) => [key, chosenItems[key]])
+            .map((keyVal, index) => (
+              <li key={index}>{keyVal[0]}</li>
+            ))}
+        </ul>
+        <button onClick={() => sendDataToDB(chosenItems)}>Submit</button>
+      </div>
 
       <Footer>
         <Link href="/">
