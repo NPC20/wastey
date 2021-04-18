aabafe8493ac99605ab47763d372af612b044ae6
Akshaya <akshayasatheesh@hotmail.co.uk>
2021-04-18 15:45:19 +0100

    Created reusable footer

---
+++ components/Footer.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Link from 'next/link';
+import HomeBtn from '../components/HomeBtn';
+import LogoutBtn from '../components/LogoutBtn';
+import { StyledFooter } from './../src/styledComponents/reusables';
+import { useAuth } from "../src/useAuth";
+
+export default function Footer() {
+  const { signout } = useAuth();
+
+  return (
+    <StyledFooter>
+      <Link href='/#' passHref>
+        <HomeBtn />
+      </Link>
+      <Link href='/' onClick={() => signout()} passHref>
+        <LogoutBtn/>
+      </Link>
+    </StyledFooter>
+  )
+}
