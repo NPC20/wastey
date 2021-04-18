263be8b3c62b4567224f054f4abdf8a0799c4e95
ephie oyedoh <ephieo@rocketmail.com>
2021-04-18 09:24:11 +0100

    creating index,shopping and waste pages

--- wastey-app/pages/waste.js
+++ wastey-app/pages/waste.js
@@ -1,5 +1,3 @@
-// import Head from 'next/head';
-// import styles from '../styles/Home.module.css';
 import Link from 'next/Link';
 import Image from 'next/image';
 import { HomeTabs, Footer } from './../src/styledComponents/reusables';
@@ -8,21 +6,13 @@ export default function Home() {
   return (
     <div className="mainCont">
       <Image
-        src="/profileImg.svg"
+        src="/binIcon.svg"
         alt="img"
         width={100}
         height={100}
         layout="fixed"
       />
-      <Link href="shopping">
-        <HomeTabs>ADD ITEMS</HomeTabs>
-      </Link>
-      <Link href="/waste">
-        <HomeTabs>ADD WASTE</HomeTabs>
-      </Link>
-      <Link href="/stats">
-        <HomeTabs>STATS</HomeTabs>
-      </Link>
+
       <Footer>
         <Link href="/">
           <Image
